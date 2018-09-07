6b41dfc90b78db87a639b72843401d59725fe65a
Mayureshk19 <37764949+Mayureshk19@users.noreply.github.com>
2018-09-07 15:07:22 +0530

    Create profile.service.ts

---
+++ C:/Users/320/Desktop/git-githubapi/src/app/services/profile.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProfileService {
+
+  private username:string;
+  private clientid = 'c7e60d4150a9713f0c2a';
+  private clientsecret = '9ef85674339300dde89df27377e8b0fe78b2c39d';
+
+  constructor(private http:Http) { 
+  	console.log("service is now ready!");
+  	this.username = 'Mayureshk19';
+  }
+
+  getProfileInfo(){
+  	return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+  	.map(res => res.json());
+  }
+
+  getProfileRepos(){
+  	return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+  	.map(res => res.json());
+  }
+
+  updateProfile(username:string){
+  	this.username = username;
+  }
+
+}
